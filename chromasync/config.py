d3b648785df51ac6d598809eab15fe04c0aaa309
daniele <daniele.monzani@outlook.com>
2024-04-05 11:52:57 +0200

    feat(config): support for env variables
    chromasync now looks for environment variables CHROMASYNC_CONFIG_DIR
    and CHROMASYNC_CACHE_DIR to set config and cache directories

--- chromasync/config.py
+++ chromasync/config.py
@@ -3,32 +3,38 @@ import os
 
 
 _CHROMASYNC_DIR_NAME = "chromasync"
+_CHROMASYNC_CONF_DIR_ENV_VAR  = "CHROMASYNC_CONFIG_DIR"
+_CHROMASYNC_CACHE_DIR_ENV_VAR = "CHROMASYNC_CACHE_DIR"
 
 
+# System directories
 _HOME_DIR      = Path(os.getenv("HOME", os.getenv("USERPROFILE")))
 _XDG_CACHE_DIR = Path(os.getenv("XDG_CACHE_HOME",  _HOME_DIR / ".cache"))
 _XDG_CONF_DIR  = Path(os.getenv("XDG_CONFIG_HOME", _HOME_DIR / ".config"))
 
 
-# Configuration directories
-CONF_DIR = Path(os.path.join(_XDG_CONF_DIR, _CHROMASYNC_DIR_NAME))
+# Default config and cache directories
+_DEFAULT_CONF_DIR  = os.path.join(_XDG_CONF_DIR,  _CHROMASYNC_DIR_NAME)
+_DEFAULT_CACHE_DIR = os.path.join(_XDG_CACHE_DIR, _CHROMASYNC_DIR_NAME)
+
+
+# Configuration dirs/files
+CONF_DIR = Path(os.getenv(_CHROMASYNC_CONF_DIR_ENV_VAR, _DEFAULT_CONF_DIR))
 TEMPLATES_DIR = CONF_DIR / "templates"
 PALETTES_DIR  = CONF_DIR / "palettes"
 POST_SCRIPT_FILE = CONF_DIR / "chromasync-post.sh"
 # CONF_FILE     = CONF_DIR / "chromasync.conf"
 
 
-# Cache directories
-CACHE_DIR  = Path(os.path.join(_XDG_CACHE_DIR, _CHROMASYNC_DIR_NAME))
+# Cache dirs/files
+CACHE_DIR  = Path(os.getenv(_CHROMASYNC_CACHE_DIR_ENV_VAR, _DEFAULT_CACHE_DIR))
 OUTPUT_DIR = CACHE_DIR / "out"
 LOG_FILE   = CACHE_DIR / "chromasync.log"
 
 
-
-
 # Builds all the required files and directories
 def build_environment():
-    # Config files and directories
+    # Builds config dirs/files
     CONF_DIR.mkdir(parents=True, exist_ok=True)
     TEMPLATES_DIR.mkdir(parents=True, exist_ok=True)
     PALETTES_DIR.mkdir(parents=True, exist_ok=True)
@@ -37,7 +43,7 @@ def build_environment():
         POST_SCRIPT_FILE.touch()
 
 
-    # Cache files and directories
+    # Builds cache dirs/files
     CACHE_DIR.mkdir(parents=True, exist_ok=True)
     OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
 
