88d135c680e4bbe4e725cafca426341b47881cb3
daniele <daniele.monzani@outlook.com>
2024-04-07 16:59:24 +0200

    refactor(directives): add a possible future change

--- chromasync/config/directives.py
+++ chromasync/config/directives.py
@@ -2,6 +2,8 @@ from enum import Enum
 
 
 class Directives(Enum):
+    # TODO: change @out to @dir (or add @dir) to specify the output
+    #       directory. The output file name is the template file name
     OUT_FILE     = "@out:"
     COLOR_FORMAT = "@fmt:"
 
